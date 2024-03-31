dd0469ff8422be4ba6b13e847551e652dc31aa7b
gin <73906888+gingkapls@users.noreply.github.com>
2024-03-31 20:52:34 +0530

    add dom interaction, add board state getters

--- script.js
+++ script.js
@@ -3,17 +3,23 @@
 const Gameboard = ((mark1, mark2) => {
   const [numRows, numCols] = [3, 3];
 
+  let currentTurn = mark1;
+  let isEnded = false;
+  let isWon = false;
+  let isFull = false;
+
   const board = Array.from({ length: numRows }, () =>
     new Array(numCols).fill(null)
   );
-
-  let currentTurn = mark1;
 
   const toggleTurn = () => {
     currentTurn = currentTurn === mark1 ? mark2 : mark1;
   };
 
   const getTurn = () => currentTurn;
+  const getIsEnded = () => isEnded;
+  const getIsWon = () => isWon;
+  const getIsFull = () => isFull;
 
   const isEmptyCell = ({ row, col }) => {
     if (board[row][col]) return false;
@@ -21,14 +27,18 @@ const Gameboard = ((mark1, mark2) => {
     return true;
   };
 
-  const getBoard = () => board.forEach((row) => console.log(row));
+  const getBoard = () => board;
 
   const resetBoard = () => {
     board.forEach((row) => row.fill(null));
+    isEnded = false;
+    isFull = false;
+    isWon = false;
     toggleTurn();
   };
 
-  const isBoardFull = () => !board.flat().some((cell) => cell === null);
+  const checkBoardFull = () =>
+    isFull || !board.flat().some((cell) => cell === null);
 
   // There is a row where there doesn't exists a cell that's not marked or it is not equal to the first cell
   const checkRowWin = () =>
@@ -47,6 +57,7 @@ const Gameboard = ((mark1, mark2) => {
 
         if (board[j][i] !== board[0][i]) win = false;
       }
+
       if (win) return win;
     }
   };
@@ -66,6 +77,7 @@ const Gameboard = ((mark1, mark2) => {
         break;
       }
     }
+
     if (win) return win;
 
     win = true;
@@ -76,7 +88,7 @@ const Gameboard = ((mark1, mark2) => {
         break;
       }
 
-      if (board[i][numRows - i - 1] !== board[numRows - 1][numCols - 1]) {
+      if (board[i][numRows - i - 1] !== board[0][numCols - 1]) {
         win = false;
         break;
       }
@@ -86,23 +98,32 @@ const Gameboard = ((mark1, mark2) => {
   };
 
   const checkWin = () => {
-    // TODO: implement win check
-    let win;
-    // check if there is a row where all cells are equal to the first cell
-    // win = checkColWin();
-    win = checkRowWin() || checkColWin() || checkDiagonalWin();
-    if (win) return { currentTurn, win };
+    if (isWon) return isWon;
 
+    isWon = checkRowWin() || checkColWin() || checkDiagonalWin();
+    if (isWon) return { currentTurn, isWon };
     return false;
   };
 
   const placeMarker = ({ row, col }) => {
-    if (!isEmptyCell({ row, col })) return "ERROR";
+    if (!isEmptyCell({ row, col })) return false;
+    if (isWon) return false;
 
-    console.log("");
     board[row][col] = currentTurn;
     getBoard();
-    console.log(checkWin());
+    if (checkWin().isWon) {
+      console.log(`Game has ended ${currentTurn} has won`);
+      isWon = true;
+      isEnded = true;
+      return true;
+    }
+
+    if (checkBoardFull()) {
+      isEnded = true;
+      isWon = false;
+      console.log(`No one won`);
+    }
+
     toggleTurn();
     return true;
   };
@@ -111,8 +132,11 @@ const Gameboard = ((mark1, mark2) => {
     resetBoard,
     getBoard,
     placeMarker,
+    isEmptyCell,
     getTurn,
-    checkWin,
+    getIsWon,
+    getIsFull,
+    getIsEnded,
   };
 })("X", "O");
 
@@ -134,8 +158,51 @@ Gameboard.placeMarker({ row: 2, col: 2 }); // O
 Gameboard.placeMarker({ row: 2, col: 1 }); // X */
 
 // Diagonal win test
-Gameboard.placeMarker({ row: 0, col: 0 }); // X
+/* Gameboard.placeMarker({ row: 0, col: 0 }); // X
 Gameboard.placeMarker({ row: 2, col: 0 }); // O
 Gameboard.placeMarker({ row: 1, col: 1 }); // X
 Gameboard.placeMarker({ row: 2, col: 1 }); // O
-Gameboard.placeMarker({ row: 2, col: 2 }); // X
+Gameboard.placeMarker({ row: 2, col: 2 }); // X */
+
+const boardElement = document.querySelector("#board");
+const resultElement = document.querySelector("#result");
+
+const renderToDom = ((boardEl, resultEl) => {
+  const initialRender = () => {
+    const board = Gameboard.getBoard();
+    board.forEach((row, i) => {
+      const rowContainer = document.createElement("div");
+      rowContainer.classList.add("row");
+
+      row.forEach((cell, j) => {
+        const cellElem = document.createElement("div");
+        cellElem.classList.add("cell");
+        cellElem.dataset.row = i;
+        cellElem.dataset.col = j;
+        rowContainer.append(cellElem);
+      });
+      boardEl.append(rowContainer);
+    });
+  };
+
+  boardEl.addEventListener("click", (event) => {
+    if (event.target === event.currentTarget) return;
+    if (event.target.classList.contains("row")) return;
+
+    const { row, col } = {
+      row: event.target.dataset.row,
+      col: event.target.dataset.col,
+    };
+
+    if (Gameboard.isEmptyCell({ row, col }) && !Gameboard.getIsWon()) {
+      event.target.textContent = Gameboard.getTurn();
+      Gameboard.placeMarker({ row, col });
+    }
+  });
+
+  return {
+    initialRender,
+  };
+})(boardElement, resultElement);
+
+renderToDom.initialRender();
